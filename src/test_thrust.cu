99ad9c1b40a7bfb28522b626989fc8ca7a3e397d
Danny Riordan <dannyriordan@gmail.com>
2016-02-09 17:39:02 +0000

    transform, reduce

--- src/test_thrust.cu
+++ src/test_thrust.cu
@@ -1,13 +1,30 @@
 #include <thrust/device_vector.h>
 #include <thrust/reduce.h>
+#include <thrust/transform.h>
+#include <thrust/functional.h>
 #include <cstdlib>
 
 using namespace std;
 using namespace thrust;
 
+struct saxpy_functor{ 
+    const float a;
+    
+    saxpy_functor(float _a):a(_a){} 
+    
+    __host__ __device__ float operator()(const float& x, const float& y) const {
+        return a * x + y; } 
+    }; 
+
 int add_up(std::vector<int> xs, int &total){
     
     device_vector<int> xs_d(xs.begin(),xs.end());
+
+    thrust::transform(
+            xs_d.begin(),xs_d.end(),
+            xs_d.begin(),
+            xs_d.begin(),
+            saxpy_functor(2));
 
     total=reduce(xs_d.begin(), xs_d.end(), 0, thrust::plus<int>());
 
